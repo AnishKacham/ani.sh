9423a28387fe4b547edf1cd489bc5a1ab6f92576
Anish Kacham <f20190091@hyderabad.bits-pilani.ac.in>
2023-10-23 13:29:11 +0530

    🚸 feat(mini-spotify): show other content when TLS error

--- src/app/(components)/mini-spotify.tsx
+++ src/app/(components)/mini-spotify.tsx
@@ -23,50 +23,63 @@ function renderEqualizer(count: number = 20, active: boolean = true) {
 }
 
 export default async function MiniSpotify() {
-  const spotifyData = await fetchLatestPlayed();
+  try {
+    const spotifyData = await fetchLatestPlayed();
 
-  return (
-    /*
+    return (
+      /*
       TODO: Figure out how to use this
       https://github.com/davidkrantz/Colorfy/blob/master/spotify_background_color.py
       to pick the perfect background color for the currently playing songs album image.
     */
-    <a
-      href={spotifyData.album.external_urls.spotify}
-      target="_blank"
-      className={clsx(
-        "flex flex-row w-36 rounded-md button gap-1 dark:bg-slate-700 bg-slate-300",
-      )}
-    >
-      <Image
-        src={spotifyData.album.images[0].url}
-        width={spotifyData.album.images[0].width}
-        height={spotifyData.album.images[0].height}
-        alt="album-image"
-        className="h-10 w-10 rounded-l-md"
-      />
-      <div className="h-full w-full">
-        <div className="flex flex-row gap-1">
-          <Image
-            src={Spotify}
-            alt="spotify"
-            width={14}
-            height={14}
-            className="self-center"
-          />
-          <div className="overflow-hidden w-20">
-            <div
-              className={clsx(
-                "text-sm whitespace-nowrap text-left w-fit dark:text-slate-200",
-                spotifyData.name.length > 10 ? "animate-marquee-mini" : "",
-              )}
-            >
-              {spotifyData.name}
+      <a
+        href={spotifyData.album.external_urls.spotify}
+        target="_blank"
+        className={clsx(
+          "flex flex-row w-36 rounded-md button gap-1 dark:bg-slate-700 bg-slate-300",
+        )}
+      >
+        <Image
+          src={spotifyData.album.images[0].url}
+          width={spotifyData.album.images[0].width}
+          height={spotifyData.album.images[0].height}
+          alt="album-image"
+          className="h-10 w-10 rounded-l-md"
+        />
+        <div className="h-full w-full">
+          <div className="flex flex-row gap-1">
+            <Image
+              src={Spotify}
+              alt="spotify"
+              width={14}
+              height={14}
+              className="self-center"
+            />
+            <div className="overflow-hidden w-20">
+              <div
+                className={clsx(
+                  "text-sm whitespace-nowrap text-left w-fit dark:text-slate-200",
+                  spotifyData.name.length > 10 ? "animate-marquee-mini" : "",
+                )}
+              >
+                {spotifyData.name}
+              </div>
             </div>
           </div>
+          <div className="flex flex-row gap-1">{renderEqualizer(16)}</div>
         </div>
-        <div className="flex flex-row gap-1">{renderEqualizer(16)}</div>
+      </a>
+    );
+  } catch (error) {
+    return (
+      <div className="flex flex-row items-center w-36 rounded-md button gap-1 bg-slate-700">
+        <div className="w-[60px] h-[40px] rounded-l-md bg-slate-600 flex justify-center items-center">
+          <Image src={Spotify} alt="spotify" width={22} height={22} />
+        </div>
+        <div className="w-full flex flex-col gap-1.5 pr-1.5 text-center">
+          <span className="font-extrabold text-sm">TLS ERR.</span>
+        </div>
       </div>
-    </a>
-  );
+    );
+  }
 }
