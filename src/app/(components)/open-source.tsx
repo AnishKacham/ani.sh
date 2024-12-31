713340c19536f378b66b7a92dbbb0b3be6183cbd
AnishKacham <f20190091@hyderabad.bits-pilani.ac.in>
2024-12-31 18:28:56 +0530

    fix(colors): wrong colors in light theme

--- src/app/(components)/open-source.tsx
+++ src/app/(components)/open-source.tsx
@@ -7,7 +7,7 @@ export default function OpenSource() {
       <div className="text-3xl font-extrabold dark:text-white flex flex-col mb-6">
         <div className="flex flex-row items-center gap-8">
           <div>Open Source Contributions</div>
-          <div className="border-t dark:border-gray-700 border-gray-200 h-0 grow" />
+          <div className="border-t dark:border-gray-700 border-black h-0 grow" />
         </div>
       </div>
 
@@ -20,17 +20,21 @@ export default function OpenSource() {
               </span>
               <span className="text-sm ml-2"> {osourceitem.month_year} </span>
             </Link>
-            <div className="w-full border p-6 rounded-md dark:border-gray-600 border-gray-200 mt-1">
+            <div className="w-full border p-6 rounded-md border-slate-300 dark:border-gray-700 mt-1">
               <Link href={osourceitem.repo_link + "/pull/" + osourceitem.pr_no}>
-                <span className="text-xl text-white">
+                <span className="text-xl dark:text-white hover:underline">
                   {" "}
                   {osourceitem.pr_title}{" "}
                 </span>
-                <span className="text-xl">#{osourceitem.pr_no}</span>
+                <span className="text-xl text-gray-400 dark:text-gray-500">
+                  #{osourceitem.pr_no}
+                </span>
               </Link>
               <div className="mt-1 ml-5">
                 <ul>
-                  <li className="text-md">{osourceitem.pr_description}</li>
+                  <li className="text-md dark:text-gray-300">
+                    {osourceitem.pr_description}
+                  </li>
                 </ul>
               </div>
             </div>
--- src/app/(components)/work-history-v2.tsx
+++ src/app/(components)/work-history-v2.tsx
@@ -9,7 +9,7 @@ export default async function WorkHistoryV2() {
       <div className="gap-14 flex flex-col w-full">
         <div className="text-3xl font-extrabold dark:text-slate-200 text-black w-full flex flex-row items-center gap-8">
           {/* <div className="border-t dark:border-gray-700 border-gray-200 h-0 grow" /> */}
-          <div className="text-center w-64 text-white">
+          <div className="text-center w-64 dark:text-white">
             Work Experience
           </div>
           <div className="border-t dark:border-gray-700 border-gray-200 h-0 grow" />
